b99a9117f17692ce629fdee279de09dc916d8fd8
XinyueZ <dev.xinyue.zhao@gmail.com>
2024-03-27 15:06:02 +0100

    finalize nvidia_vs_groq

--- simple/nvidia_vs_groq.py
+++ simple/nvidia_vs_groq.py
@@ -14,7 +14,6 @@ from langchain.text_splitter import SentenceTransformersTokenTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_core.language_models import BaseChatModel
 from langchain_core.prompts.chat import ChatPromptTemplate
-from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_core.retrievers import BaseRetriever
 from langchain_core.runnables import RunnablePassthrough, chain
 from langchain_core.runnables.base import RunnableSerializable
@@ -55,23 +54,36 @@ llm_temperature = st.sidebar.slider(
     "LLM temperature for RAG",
     0.0,
     1.0,
-    0.5,
+    0.0,
     key="llm_temperature_slider",
 )
 
+hypo_gen_temperature = st.sidebar.slider(
+    "HyDE temperature",
+    0.0,
+    1.0,
+    0.8,
+    key="hypo_gen_temperature_slider",
+)
 
-def llm_selector(model_name: str) -> BaseChatModel:
+
+def llm_selector(
+    model_name: str,
+    temperature: float,
+) -> BaseChatModel:
     model_brand = model_name.split(" ")[0]
     match model_brand:
         case "Groq":
             return ChatGroq(
                 model=model_map[model_name],
-                temperature=llm_temperature,
+                temperature=temperature,
+                max_tokens=2048 * 2,
             )
         case "Nvidia":
             return ChatNVIDIA(
                 model=model_map[model_name],
-                temperature=llm_temperature,
+                temperature=temperature,
+                max_tokens=2048 * 2,
             )
         case _:
             raise ValueError(f"Model {model_name} not found")
@@ -83,7 +95,8 @@ llm_name_selector = st.sidebar.selectbox(
     index=2,
     key="llm_selector",
 )
-llm = llm_selector(llm_name_selector)
+llm = llm_selector(llm_name_selector, llm_temperature)
+hyde_llm = llm_selector(llm_name_selector, hypo_gen_temperature)
 
 
 def create_retriever(file_path: str) -> BaseRetriever:
@@ -94,87 +107,89 @@ def create_retriever(file_path: str) -> BaseRetriever:
     return db.as_retriever()
 
 
-def router_chain() -> RunnableSerializable:
+def route_chain() -> RunnableSerializable:
     prompt = ChatPromptTemplate.from_messages(
         [
             SystemMessagePromptTemplate.from_template(
-                """Given a conversation history to decide what next-step will follow, "standardalonequestion"  or "summary".
-"standardalonequestion" if the conversation is just started.
-"summary" if the conversation is ongoing (more than one interaction between the human and the AI).
-Notice: Only return eithter "standardalonequestion" or "summary" without any instruction text, reasoning text, headlines, leading-text or other additional information.
+                """As an AI assistant for application route, determine whether to enter the process for handling declarative and normal sentences or interrogative and question sentences (indirect or direct questions) based on the user's query tone inside [Query] marks,
+If it is a declarative and normal sentence process, return the string: "hydechain"; 
+otherwise, return: "standardalonequery".
+
+Notice: 
+Do not answer the query or make up the query, only return as simple as possible, eithter "standardalonequery" or "hydechain" as string without any instruction text, reasoning text, headlines, leading-text or other additional information.
 """
+            ),
+            HumanMessagePromptTemplate.from_template("""[Query]{query}[Query]"""),
+        ]
+    )
+
+    return prompt | llm | StrOutputParser()
+
+
+def hyde_chain() -> RunnableSerializable:
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            SystemMessagePromptTemplate.from_template(
+                """Generate a different version of the text inside [Origin text] marks and conversation history to retrieve relevant documents from a vector database.
+The version is for better model comprehension while maintaining the original text sentiment and brevity.
+Your goal is to help the user overcome some of the limitations of the distance-based similarity search. 
+Notice: Only return the reformulated statement without any explaination or additional information."""
             ),
             MessagesPlaceholder(variable_name="history"),
             HumanMessagePromptTemplate.from_template(
-                """What is the next step? (standardalonequestion or summary) and what is the conversation about based on history?"""
+                "[Origin text]\n{query}\n[Origin text]"
             ),
         ]
     )
 
-    return prompt | llm | StrOutputParser()
+    return prompt | hyde_llm | StrOutputParser()
 
 
-def summary_chain() -> RunnableSerializable:
+def standalone_query_chain() -> RunnableSerializable:
     prompt = ChatPromptTemplate.from_messages(
         [
             SystemMessagePromptTemplate.from_template(
-                """Given a chat history and a follow-up question. Summerize the conversation based on the chat history and the follow-up question.
-If the history is empty, do nothing just return the follow-up question as the summary.
-Notice: Only return the final summary without any instruction text, headlines, leading-text or other additional information."""
+                """Given a chat history and a follow-up query inside [Query] marks, rephrase the follow-up query to be a standalone query. \
+Do NOT answer the query, just reformulate it if needed, otherwise return it as is.
+Notice: Only return the final standalone query without any instruction text, headlines, leading-text or other additional information."""
             ),
             MessagesPlaceholder(variable_name="history"),
-            HumanMessagePromptTemplate.from_template("{question}"),
+            HumanMessagePromptTemplate.from_template("[Query]{query}[Query]"),
         ]
     )
 
     return prompt | llm | StrOutputParser()
 
 
-def standalone_question_chain() -> RunnableSerializable:
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            SystemMessagePromptTemplate.from_template(
-                """Given a chat history and a follow-up question, rephrase the follow-up question to be a standalone question. \
-Do NOT answer the question, just reformulate it if needed, otherwise return it as is.
-Notice: Only return the final standalone question without any instruction text, headlines, leading-text or other additional information."""
-            ),
-            MessagesPlaceholder(variable_name="history"),
-            HumanMessagePromptTemplate.from_template("{question}"),
-        ]
-    )
-
-    return prompt | llm | StrOutputParser()
-
-
-@chain
-def route(info) -> RunnableSerializable:
-    pretty_print("info", info)
-    if "standardalonequestion" in info["next_step"]:
-        pretty_print("standalone_question_chain", info["next_step"])
-        return standalone_question_chain()
-
-    if "summary" in info["next_step"]:
-        pretty_print("summary", info["next_step"])
-        return summary_chain()
-
-
-def create_chain(
+def build_final_chain(
     base_retriever: BaseRetriever,
 ) -> RunnableSerializable:
+    @chain
+    def _routed_chain_(info) -> RunnableSerializable:
+        pretty_print("info", info)
+        if "standardalonequery" in info["next_step"].lower():
+            pretty_print("standalone_query_chain")
+            return standalone_query_chain()
+
+        if "hydechain" in info["next_step"].lower():
+            pretty_print("hydechain")
+            return hyde_chain()
+
+        raise ValueError("Invalid next step")
+
     prompt = ChatPromptTemplate.from_messages(
         [
             SystemMessagePromptTemplate.from_template(
-                "Answer question solely based on the following context:\n{context}\n"
+                "Answer query inside [Query] marks solely based on the following context:\n{context}\n"
             ),
             MessagesPlaceholder(variable_name="history"),
-            HumanMessagePromptTemplate.from_template("{question}"),
+            HumanMessagePromptTemplate.from_template("[Query]{query}[Query]"),
         ]
     )
-    # RunnablePassthrough.assign(context=standalone_question_chain() | base_retriever)
-    # RunnablePassthrough.assign(context=summary_chain() | base_retriever)
+
     mid_chain = (
-        RunnablePassthrough.assign(next_step=router_chain())
-        | RunnablePassthrough.assign(context=(route | base_retriever))
+        RunnablePassthrough.assign(next_step=route_chain())
+        | RunnablePassthrough.assign(context=(_routed_chain_ | base_retriever))
         | prompt
         | llm
         | StrOutputParser()
@@ -183,7 +198,7 @@ def create_chain(
     final_chain = RunnableWithMessageHistory(
         mid_chain,
         lambda _: st.session_state["history"],
-        input_messages_key="question",
+        input_messages_key="query",
         history_messages_key="history",
     )
     return final_chain | StrOutputParser()
@@ -227,21 +242,21 @@ def doc_uploader() -> BaseRetriever | None:
 def main():
     st.sidebar.title("Upload file")
     st.sidebar.write(
-        "[Suggest this file to ask some questions](https://dl.dropbox.com/scl/fi/xojn7rk5drda8ba4i90xr/4b1ca7c6-b279-4ed9-961a-484cadf8dd16.pdf?rlkey=aah3wklftddsgw7g5lrkv2tg4&dl=0)"
+        "[Suggest this file to query](https://dl.dropbox.com/scl/fi/xojn7rk5drda8ba4i90xr/4b1ca7c6-b279-4ed9-961a-484cadf8dd16.pdf?rlkey=aah3wklftddsgw7g5lrkv2tg4&dl=0)"
     )
     retriever: BaseRetriever | None = doc_uploader()
     if retriever is None:
         return
-    question = st.text_input(
-        "Question",
-        key="question",
-        placeholder="Enter your question here",
+    query = st.text_input(
+        "Query",
+        key="query",
+        placeholder="Enter your query here",
     ).strip()
 
-    if question is not None and question != "":
-        final_chain = create_chain(base_retriever=retriever)
+    if query is not None and query != "":
+        final_chain = build_final_chain(base_retriever=retriever)
         response = final_chain.stream(
-            {"question": question},
+            {"query": query},
             {"configurable": {"session_id": None}},
         )
         st.write_stream(response)

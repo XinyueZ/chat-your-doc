01c680150bce1244c5f1044a98fb2624dbd9e038
ChrisDEV <dev.xinyue.zhao@gmail.com>
2024-05-29 16:29:29 +0000

    add agent to multimodal chat

--- simple/multimodal_chat.py
+++ simple/multimodal_chat.py
@@ -18,17 +18,28 @@ from langchain.agents.agent import AgentExecutor
 from langchain.agents.structured_chat.base import create_structured_chat_agent
 from langchain.memory import ChatMessageHistory
 from langchain.prompts import HumanMessagePromptTemplate, MessagesPlaceholder
+from langchain.schema.output_parser import StrOutputParser
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.tools import tool
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_community.tools.wikidata.tool import (WikidataAPIWrapper,
+                                                     WikidataQueryRun)
+from langchain_community.tools.wikipedia.tool import WikipediaQueryRun
+from langchain_community.utilities import GoogleSearchAPIWrapper
+from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
+from langchain_community.vectorstores import FAISS
 from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_core.messages import BaseMessage, HumanMessage, SystemMessage
 from langchain_core.messages.tool import ToolMessage, ToolMessageChunk
 from langchain_core.prompts import PromptTemplate
 from langchain_core.prompts.chat import ChatPromptTemplate
 from langchain_core.pydantic_v1 import BaseModel, Field
-from langchain_core.runnables import Runnable
+from langchain_core.runnables import Runnable, RunnablePassthrough
 from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.tools import Tool
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from PIL import Image, UnidentifiedImageError
 from rich.pretty import pprint
 from ultralytics import YOLO
@@ -59,46 +70,6 @@ def pretty_print(title: str = "Untitled", content: Any = None):
         f":--> {title} --> {info.filename} --> {info.function} --> line: {info.lineno} --:"
     )
     pprint(content)
-
-
-def create_chain(model: BaseChatModel, base64_image: bytes) -> Runnable:
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            SystemMessage(
-                content=[
-                    {
-                        "type": "text",
-                        "text": """As a helpful assistant, you should respond to the user's query.""",
-                    }
-                ]
-            ),
-            MessagesPlaceholder(variable_name="history"),
-            HumanMessagePromptTemplate.from_template(
-                template=(
-                    [
-                        {
-                            "type": "text",
-                            "text": "{query}",
-                        },
-                        {
-                            "type": "image_url",
-                            "image_url": {
-                                "url": f"data:image/jpeg;base64,{base64_image}",
-                            },
-                        },
-                    ]
-                    if base64_image
-                    else [
-                        {
-                            "type": "text",
-                            "text": "{query}",
-                        },
-                    ]
-                )
-            ),
-        ]
-    )
-    return prompt | model
 
 
 def image_url_to_image(image_url: str) -> Image:
@@ -148,7 +119,134 @@ def doc_uploader() -> bytes:
         return None
 
 
+# ------------------------------------------LLM Chain------------------------------------------
+
+
+def create_chain(model: BaseChatModel, base64_image: bytes) -> Runnable:
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            SystemMessage(
+                content=[
+                    {
+                        "type": "text",
+                        "text": """As a helpful assistant, you should respond to the user's query.""",
+                    }
+                ]
+            ),
+            MessagesPlaceholder(variable_name="history"),
+            HumanMessagePromptTemplate.from_template(
+                template=(
+                    [
+                        {
+                            "type": "text",
+                            "text": "{query}",
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{base64_image}",
+                            },
+                        },
+                    ]
+                    if base64_image
+                    else [
+                        {
+                            "type": "text",
+                            "text": "{query}",
+                        },
+                    ]
+                )
+            ),
+        ]
+    )
+    return prompt | model
+
+
+# ------------------------------------------agent, functions, tools------------------------------------------
+class LoadUrlsTool(BaseModel):
+    query: str = Field(description="The query to search for.")
+    urls: List[str] = Field(description="The URLs to load.")
+
+
+load_urls_prompt_template = PromptTemplate.from_template(
+    """Reponse the query inside [query] based on the context inside [context]:
+
+[query]
+{query}
+[query]
+
+[context]
+{context}
+[context]
+
+Only return the answer without any instruction text or additional information.
+Keep the result as simple as possible."""
+)
+
+
+@tool("load-urls-tool", args_schema=LoadUrlsTool, return_direct=False)
+def load_urls_tool(retrieval_model: BaseChatModel, query: str, urls: List[str]) -> str:
+    """Load the content of the given Urls and return the query result."""
+    loader = WebBaseLoader(urls)
+    docs = loader.load()
+    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000, chunk_overlap=0
+    )
+    chunks = splitter.split_documents(docs)
+    db = FAISS.from_documents(chunks, OpenAIEmbeddings())
+    retriever = db.as_retriever()
+
+    chain = (
+        {"context": retriever, "query": RunnablePassthrough()}
+        | load_urls_prompt_template
+        | retrieval_model
+        | StrOutputParser()
+    )
+    return chain.invoke(query)
+
+
+agent_tools = [
+    Tool(
+        name="Google Search",
+        description="Search Google for recent results.",
+        func=GoogleSearchAPIWrapper(
+            google_api_key=os.environ.get("GOOGLE_CSE_KEY")
+        ).run,
+    ),
+    Tool(
+        name="DuckDuckGo Search",
+        func=DuckDuckGoSearchRun().run,
+        description="Use to search for the information from DuckDuckGo.",
+    ),
+    Tool(
+        name="Wikipedia Search",
+        func=WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper()).run,
+        description="Use to search for the information from Wikipedia.",
+    ),
+    Tool(
+        name="Wikidata Search",
+        func=WikidataQueryRun(api_wrapper=WikidataAPIWrapper()).run,
+        description="Use to search for the information from Wikidata.",
+    ),
+]
+
+
+def create_agent(agent_model: BaseChatModel) -> AgentExecutor:
+    return AgentExecutor(
+        agent=create_structured_chat_agent(
+            llm=agent_model,
+            tools=agent_tools,
+            prompt=hub.pull("hwchase17/structured-chat-agent"),
+        ),
+        tools=agent_tools,
+        handle_parsing_errors=True,
+        return_intermediate_steps=True,
+    )
+
+
 # ------------------------------------------tool functions------------------------------------------
+
+
 def generate_image(model: BaseChatModel, context: str) -> str:
     """Generate an image to illustrate for user request."""
 
@@ -225,6 +323,23 @@ def annotate_image(
     return results, save_path
 
 
+def run_agent(agent: AgentExecutor, known_topic: str) -> Dict[str, Any]:
+    """Run the agent for the unknown topic, the agent will search the topic through the web and return the result."""
+    prompt = f"""Search through the internet for the unknown topic inside >>>>>>>>> and <<<<<<<<<<
+>>>>>>>>>
+{known_topic}
+<<<<<<<<<<
+We recommend certain tools that you can use:
+-  Google Search
+-  DuckDuckGo Search
+-  Wikipedia Search
+-  Wikidata Search
+also if you face some urls you can also use the tool:
+- load-urls-tool
+"""
+    return agent.invoke({"input": prompt})
+
+
 # ------------------------------------------tool------------------------------------------
 class GenerateImageTool(BaseModel):
     """Generate an image to illustrate for user request."""
@@ -244,6 +359,15 @@ class AnnotateImageTool(BaseModel):
     )
 
 
+class RunAgentTool(BaseModel):
+    """Run the agent for the unknown topic, an agent will search the topic through the web and return the result."""
+
+    known_topic: str = Field(
+        ...,
+        description="The known topic to search for the unknown topic, an agent will use this topic to search through the web.",
+    )
+
+
 # ------------------------------------------model event handlers------------------------------------------
 def handle_generate_image(
     tool_name: str,
@@ -252,15 +376,19 @@ def handle_generate_image(
     context: str,
     image_width=500,
 ) -> Dict[str, str]:
+    additional_kwargs = dict()
     try:
         func = FUN_MAPPING.get(tool_name, None)
-        image, image_url = func(context=context) if func else "No tool provided."
+        if func:
+            image, image_url = func(context=context)
+        else:
+            raise ValueError("No tool provided.")
+
         if image and image_url:
             st.image(image, width=image_width)
         else:
             st.write("No image generated.")
-        # Finished tool call with a function, add result to history,
-        # The model needs it for future interaction.
+
         additional_kwargs = {"image_url": image_url} if image_url else {}
 
         history_messages.append(
@@ -297,11 +425,13 @@ def handle_annotate_image(
     additional_kwargs = {}
     try:
         func = FUN_MAPPING.get(tool_name, None)
-        _, image_path = (
-            func(base64_image=base64_image, image_description=image_description)
-            if func
-            else "No tool provided."
-        )
+        if func:
+            _, image_path = func(
+                base64_image=base64_image, image_description=image_description
+            )
+        else:
+            raise ValueError("No tool provided.")
+
         if image_path:
             st.image(image_path, width=image_width)
             additional_kwargs["image_path"] = image_path
@@ -324,6 +454,45 @@ def handle_annotate_image(
         history_messages.append(
             ToolMessage(
                 content=f"Tool was called but failed to annotate image.\n\n{e}",
+                tool_call_id=tool_id,
+            )
+        )
+    return additional_kwargs
+
+
+def handle_agent_for_unknown_topic(
+    tool_name: str,
+    tool_id: str,
+    history_messages: List[BaseMessage],
+    known_topic: str,
+):
+    additional_kwargs = {}
+    try:
+        func = FUN_MAPPING.get(tool_name, None)
+        if func:
+            agent_res = func(known_topic)
+        else:
+            raise ValueError("No tool provided.")
+
+        st.write(agent_res["output"])
+        additional_kwargs["string"] = agent_res["output"]
+
+        history_messages.append(
+            ToolMessage(
+                content=(
+                    f"Tool called and get result successfully."
+                    if agent_res["output"] and agent_res["output"] != ""
+                    else "Tool called, nothing was responsed by agent."
+                ),
+                tool_call_id=tool_id,
+                additional_kwargs={"string": agent_res["output"]},
+            )
+        )
+    except Exception as e:
+        st.write(f"Something went wrong.\n\n{e}")
+        history_messages.append(
+            ToolMessage(
+                content=f"Tool was called but failed to get result.\n\n{e}",
                 tool_call_id=tool_id,
             )
         )
@@ -358,6 +527,15 @@ def tool_call_proc(
                 base64_image,
                 image_description,
                 image_width,
+            )
+            return additional_kwargs
+        case "RunAgentTool":
+            known_topic = tool_call["args"]["known_topic"]
+            additional_kwargs = handle_agent_for_unknown_topic(
+                tool_name,
+                tool_id,
+                history_messages,
+                known_topic,
             )
             return additional_kwargs
         case _:
@@ -463,6 +641,7 @@ async def main():
             temperature=st.session_state.key_temperature,
             max_tokens=MAX_TOKEN,
         )
+    ######################## config tool-bindings ########################
     partial_generate_image = partial(
         generate_image,
         model=ChatOpenAI(
@@ -475,9 +654,16 @@ async def main():
         annotate_image,
         used_model,
     )
+    partial_run_agent = partial(run_agent, create_agent(used_model))
+    agent_tools.extend([partial(load_urls_tool, used_model)])
+
     FUN_MAPPING["GenerateImageTool"] = partial_generate_image
     FUN_MAPPING["AnnotateImageTool"] = partial_annotate_image
-    used_model = used_model.bind_tools([GenerateImageTool, AnnotateImageTool])
+    FUN_MAPPING["RunAgentTool"] = partial_run_agent
+    used_model = used_model.bind_tools(
+        [GenerateImageTool, AnnotateImageTool, RunAgentTool]
+    )
+    ########################################################################
     chat_with_model(
         used_model,
         base64_image,
@@ -486,6 +672,14 @@ async def main():
     )
     streaming = st.sidebar.checkbox("Streamming", True, key="key_streaming")
     image_width = st.sidebar.slider("Image Width", 100, 1000, 500, 100, key="key_width")
+    if not os.environ.get("GOOGLE_CSE_ID") or not os.environ.get("GOOGLE_CSE_KEY"):
+        st.warning(
+            """For Google Search, set GOOGLE_CSE_ID, details: 
+key: https://developers.google.com/custom-search/docs/paid_element#api_key
+; what: https://support.google.com/programmable-search/answer/12499034?hl=en
+; enable: https://console.cloud.google.com/apis/library/customsearch.googleapis
+"""
+        )
 
 
 if __name__ == "__main__":

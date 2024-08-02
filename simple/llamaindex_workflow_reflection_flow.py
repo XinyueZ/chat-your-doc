158aa8d66f36e94fff8e4db7a01de591d1b669ca
ChrisDEV <7869833+XinyueZ@users.noreply.github.com>
2024-08-02 18:06:30 +0200

    Update llamaindex_workflow_reflection_flow.py
    Remove description with target-lang at reflection

--- simple/llamaindex_workflow_reflection_flow.py
+++ simple/llamaindex_workflow_reflection_flow.py
@@ -182,8 +182,7 @@ When writing suggestions, pay attention to whether there are ways to improve the
 (i) accuracy (by correcting errors of addition, mistranslation, omission, or untranslated text),\n\
 (ii) fluency (by applying {target_lang} grammar, spelling and punctuation rules, and ensuring there are no unnecessary repetitions),\n\
 (iii) style (by ensuring the translations reflect the style of the source text and take into account any cultural context),\n\
-(iv) terminology (by ensuring terminology use is consistent and reflects the source text domain; and by only ensuring you use equivalent idioms {target_lang}),\n\
-(iv) Use {target_lang} to describe the suggestions.\n\
+(iv) terminology (by ensuring terminology use is consistent and reflects the source text domain; and by only ensuring you use equivalent idioms {target_lang}).\n\
 Write a list of specific, helpful and constructive suggestions for improving the translation.
 Each suggestion should address one specific part of the translation.
 Output only the suggestions and nothing else."""

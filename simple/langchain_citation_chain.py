4abfbda31f7dfdfefa6d47ac18a283ad1624303f
ChrisDEV <dev.xinyue.zhao@gmail.com>
2024-08-16 19:58:17 +0000

    improve prompting, use diff spllting for init docs and chunkcing

--- simple/langchain_citation_chain.py
+++ simple/langchain_citation_chain.py
@@ -2,15 +2,13 @@
 import os
 import sys
 from dataclasses import dataclass
-from typing import (Any, Dict, Iterable, List, Literal, Optional, Sequence,
-                    Tuple, cast)
+from typing import Any, Dict, Iterable, List, Literal, Optional, Sequence, Tuple, cast
 
 from icecream import ic
 from langchain import hub
 from langchain.prompts import PromptTemplate
 from langchain.schema.output_parser import StrOutputParser
-from langchain_community.document_loaders import (UnstructuredURLLoader,
-                                                  WebBaseLoader)
+from langchain_community.document_loaders import UnstructuredURLLoader, WebBaseLoader
 from langchain_community.embeddings.ollama import OllamaEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.documents import BaseDocumentTransformer, Document
@@ -19,14 +17,14 @@ from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_core.prompts.chat import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_experimental.text_splitter import SemanticChunker
-from langchain_google_genai import (ChatGoogleGenerativeAI,
-                                    GoogleGenerativeAIEmbeddings)
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_groq import ChatGroq
 from langchain_mistralai import MistralAIEmbeddings
 from langchain_nomic.embeddings import NomicEmbeddings
 from langchain_openai import OpenAIEmbeddings
-from langchain_text_splitters.sentence_transformers import \
-    SentenceTransformersTokenTextSplitter
+from langchain_text_splitters.sentence_transformers import (
+    SentenceTransformersTokenTextSplitter,
+)
 from loguru import logger
 from rich.pretty import pprint as pp
 
@@ -36,9 +34,9 @@ from rich.pretty import pprint as pp
 # %%
 MAX_TOKEN = 2048
 TEMPERATURE = 0.0
-RETRIEVER_K = 5
-DEFAULT_CITATION_CHUNK_SIZE = 512
-DEFAULT_CITATION_CHUNK_OVERLAP = 20
+RETRIEVER_K = 20
+DEFAULT_CITATION_CHUNK_SIZE = 1024
+DEFAULT_CITATION_CHUNK_OVERLAP = 0
 # %%
 
 
@@ -72,10 +70,11 @@ module_map = {"google": GoogleModule, "groq": GroqModule}
 current_module = module_map["groq"]()
 
 
-transformer = SemanticChunker(current_module.embed)
-# SentenceTransformersTokenTextSplitter(
-#     chunk_overlap=DEFAULT_CITATION_CHUNK_OVERLAP, chunk_size=DEFAULT_CITATION_CHUNK_SIZE
-# )
+retriever_transformer = SemanticChunker(current_module.embed)
+chunking_transformer = SentenceTransformersTokenTextSplitter(
+    chunk_overlap=DEFAULT_CITATION_CHUNK_OVERLAP,
+    chunk_size=DEFAULT_CITATION_CHUNK_SIZE,
+)
 # SemanticChunker(current_module.embed)
 
 
@@ -87,11 +86,24 @@ class CitationResult:
 
 
 class CitationChain:
+
     def __init__(
-        self, current_module: ModelModule, transformer: BaseDocumentTransformer
+        self,
+        current_module: ModelModule,
+        retriever_transformer: BaseDocumentTransformer,
+        chunking_transformer: BaseDocumentTransformer,
     ):
+        """
+        Initialize a MyClass object.
+
+        Args:
+            current_module (ModelModule): The module of models.
+            retriever_transformer (BaseDocumentTransformer): The retriever transformer, for init doc splitting.
+            chunking_transformer (BaseDocumentTransformer): The chunking transformer, for citation chunking.
+        """
         self.current_module = current_module
-        self.transformer = transformer
+        self.retriever_transformer = retriever_transformer
+        self.chunking_transformer = chunking_transformer
         self.citation_prompt = ChatPromptTemplate.from_messages(
             [
                 (
@@ -112,7 +124,7 @@ class CitationChain:
                         "Answer: Water will be wet when the sky is red [2], "
                         "which occurs in the evening [1].\n"
                         "[1] means from Source 1, [2] means from Source 2."
-                        "You must always display the citation source numbers correctly between the reference location and the source location."
+                        "You must always display the citation source numbers correctly between the reference location and the source location. "
                         "Now it's your turn. Below are several numbered sources of information:"
                         "\n------\n"
                         "Context: {context}"
@@ -144,11 +156,12 @@ class CitationChain:
                         "Answer: Water will be wet when the sky is red [2], "
                         "which occurs in the evening [1].\n"
                         "[1] means from Source 1, [2] means from Source 2."
-                        "You must always display the citation source numbers correctly between the reference location and the source location."
+                        "You must always display the citation source numbers correctly between the reference location and the source location. "
                         "Now it's your turn. "
                         "We have provided an existing answer: {existing_answer}"
                         "Below are several numbered sources of information. "
                         "Use them to refine the existing answer. "
+                        "Check if the citations between reference and source are correct and if the answer is accurate, if the reference is pointed to a unknow source, please remove the citation number.  "
                         "If the provided sources are not helpful, you will repeat the existing answer."
                         "\nBegin refining!"
                         "\n------\n"
@@ -171,7 +184,7 @@ class CitationChain:
     def retrieve(self, query: str, urls: Sequence[str]) -> Sequence[Document]:
         """Retrieve the docs for the content of the given Urls."""
         docs = self.load_docs(
-            urls, lambda loader: loader.load_and_split(self.transformer)
+            urls, lambda loader: loader.load_and_split(self.retriever_transformer)
         )
 
         db = FAISS.from_documents(docs, self.current_module.embed)
@@ -195,9 +208,9 @@ class CitationChain:
             meta_title = meta.get("title", "")
             meta_language = meta.get("language", "")
 
-            doc2chunks = self.transformer.split_documents([retrieved_doc])
+            doc2chunks = self.chunking_transformer.split_documents([retrieved_doc])
             for chunk in doc2chunks:
-                page_content = f"Source {len(chunks)+1}:\n{meta_title}\n{chunk.page_content}\n{meta_source}\n"
+                page_content = f"Source {len(chunks)+1}:\n{chunk.page_content}\n"
                 new_chunk = Document(
                     page_content=page_content,
                     metadata={
@@ -235,25 +248,35 @@ class CitationChain:
             | self.current_module.llm
             | StrOutputParser()
         )
-        str_res = chain.invoke(input={"question": question, "context": context})
         citations = list(map(lambda chunk: chunk.page_content, context))
+        str_res = chain.invoke(
+            input={"question": question, "context": "\n".join(citations)}
+        )
 
         del chunks, docs
         return CitationResult(result=str_res, citations=citations)
 
 
 # %%
-citation_chain = CitationChain(current_module, transformer)
-result = citation_chain.qa(
-    (
-        "An in-depth introduction to the German capital city, "
-        "answered for those without any prior knowledge."
-    ),
-    ["https://en.wikipedia.org/wiki/Germany", "https://en.wikipedia.org/wiki/Berlin"],
-)
-ic(result.result)
-pp(result.citations)
+def main():
+    citation_chain = CitationChain(
+        current_module, retriever_transformer, chunking_transformer
+    )
+    result = citation_chain.qa(
+        (
+            "An in-depth introduction to Germany, about its culture, economy, and industry. "
+            "answered for those without any prior knowledge."
+        ),
+        [
+            "https://en.wikipedia.org/wiki/Germany",
+        ],
+    )
+    ic(result.result)
+    pp(result.citations)
+
 
 # %%
+if __name__ == "__main__":
+    main()
 
 # %%
